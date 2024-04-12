e28eab9c4df6e598f60f7f3b64ad4b142439e813
parom <E.Parominskiy@isida.by>
2024-04-12 17:43:51 +0300

    if

---
+++ src/main/com/study/kotlin/myself/flowcontrol/if/If.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val night = true;
+    if (night) {
+        println("Sleep, it is night time")
+    } else {
+        println("Be active, it is day time")
+    }
+}
