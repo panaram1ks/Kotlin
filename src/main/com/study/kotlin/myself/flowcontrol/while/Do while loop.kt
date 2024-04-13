253a4de86e612c53ae6c7f865a0336d1a66d1c00
evgen <parom.evgen@mail.ru>
2024-04-13 19:08:00 +0300

    19 While loop

---
+++ src/main/com/study/kotlin/myself/flowcontrol/while/Do while loop.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var i = 0
+    do {
+        i++
+        println(i)
+    } while (i < 10)
+}
---
+++ src/main/com/study/kotlin/myself/flowcontrol/while/While loop.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var puzzlePieces = 20
+    var piecesPlaces = 0
+    while (piecesPlaces < puzzlePieces) {
+        println(piecesPlaces++)
+    }
+}
