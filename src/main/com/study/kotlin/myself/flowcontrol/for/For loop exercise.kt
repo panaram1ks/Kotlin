e9b09b95415c16cb4fd52d07f30f8d5e4e9f4429
evgen <parom.evgen@mail.ru>
2024-04-13 18:38:41 +0300

    18 for loop

---
+++ src/main/com/study/kotlin/myself/flowcontrol/for/For loop exercise.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>){
+    for( num in 1..10){
+        println("String $num")
+    }
+
+    for(i in 10 downTo 0 step 3){
+        println(i)
+    }
+}
---
+++ src/main/com/study/kotlin/myself/flowcontrol/for/for loop.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    val animals = arrayListOf("cat", "dog", "mouse", "bear")
+    for (animal in animals) {
+        println("Feed the $animal")
+    }
+}
